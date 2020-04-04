5df6fd266d4de37ef51918e626b4db1ed98524e4
amosbastian <amosbastian@gmail.com>
2020-04-04 22:54:13 +0200

    Use create-react-app and add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "react/display-name": "off"
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
+};
