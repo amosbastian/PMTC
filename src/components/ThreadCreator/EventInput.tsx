d6aac80dea55cc9a75f6cc860a4d577cad4ecd12
amosbastian <amosbastian@gmail.com>
2020-04-05 23:31:24 +0200

    Add ThreadCreator page and EventInput component

---
+++ src/components/ThreadCreator/EventInput.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import TextInput from "../TextInput";
+import Select from "../Select";
+
+const EventInputContainer = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template-areas:
+    "event"
+    "lolesports"
+    "gamepedia"
+    "liquipedia"
+    "context";
+
+  #event {
+    grid-area: event;
+  }
+
+  #lolesports {
+    grid-area: lolesports;
+  }
+
+  #gamepedia {
+    grid-area: gamepedia;
+  }
+
+  #liquipedia {
+    grid-area: liquipedia;
+  }
+
+  #context {
+    grid-area: context;
+  }
+`;
+
+const EventInput: React.FC = () => {
+  return (
+    <EventInputContainer>
+      <Select id="event" label="Event" />
+      <TextInput id="lolesports" label="LoL eSports" placeholder="https://eu.lolesports.com/en/league/..." />
+      <TextInput id="gamepedia" label="Gamepedia" placeholder="https://lol.gamepedia.com/..." />
+      <TextInput id="liquipedia" label="Liquipedia" placeholder="http://liquipedia.net/leagueoflegends/..." />
+      <TextInput id="context" label="Context" placeholder="..." />
+    </EventInputContainer>
+  );
+};
+
+export default EventInput;
---
+++ src/pages/ThreadCreator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventInput from "../components/ThreadCreator/EventInput";
+
+const ThreadCreator: React.FC = () => {
+  return (
+    <div>
+      <EventInput />
+    </div>
+  );
+};
+
+export default ThreadCreator;
