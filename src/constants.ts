79e98530731df08723857acab2cb085ffc3388a2
amosbastian <amosbastian@gmail.com>
2020-04-05 16:08:15 +0200

    Add ROUTES constant

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const ROUTES = [
+  {
+    name: "Schedule",
+    to: "/schedule",
+  },
+  {
+    name: "Events",
+    to: "/events",
+  },
+  {
+    name: "Teams",
+    to: "/teams",
+  },
+];
