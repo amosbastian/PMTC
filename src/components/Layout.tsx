99e4983372e7ecbf3b04731f4772cb7f6fbbd5cc
amosbastian <amosbastian@gmail.com>
2020-04-09 22:16:39 +0200

    Add main to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import styled from "styled-components";
 import Header from "./Header";
+
+const Main = styled.main`
+  padding: 2rem 0;
+`;
 
 const Layout: React.FC = ({ children }) => (
   <div>
     <Header />
-    {children}
+    <Main>{children}</Main>
   </div>
 );
 
