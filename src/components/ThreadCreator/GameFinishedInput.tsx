ff0d6dec3ce7cc4eb3d411835fe820b381662845
amosbastian <amosbastian@gmail.com>
2020-04-11 12:13:35 +0200

    Add inputs towers and gold

--- src/components/ThreadCreator/GameFinishedInput.tsx
+++ src/components/ThreadCreator/GameFinishedInput.tsx
@@ -9,6 +9,55 @@ import { TeamsInputTeamsData } from "./TeamsInput/fragments";
 const GameInputFooter = styled.footer`
   display: grid;
   gap: 1rem;
+  grid-template-areas:
+    "winner time"
+    "gold1 towers1"
+    "gold2 towers2"
+    "screenshot screenshot"
+    "matchHistory matchHistory"
+    "breakdown breakdown"
+    "context context";
+
+  #winner {
+    grid-area: winner;
+  }
+
+  #time {
+    grid-area: time;
+  }
+
+  #gold1 {
+    grid-area: gold1;
+  }
+
+  #towers1 {
+    grid-area: towers1;
+  }
+
+  #gold2 {
+    grid-area: gold2;
+  }
+
+  #towers2 {
+    grid-area: towers2;
+  }
+
+  #screenshot {
+    grid-area: screenshot;
+  }
+
+  #matchHistory {
+    grid-area: matchHistory;
+  }
+
+  #breakdown {
+    grid-area: breakdown;
+  }
+
+  #context {
+    grid-area: context;
+  }
+
   padding: 0 1rem;
 `;
 
@@ -32,7 +81,31 @@ const GameFinishedInput: React.FC<GameFinishedInputProps> = ({ game, teams }) =>
   return (
     <GameInputFooter>
       <FastField id="winner" name={`games[${game}].winner`} component={Select} label="Winner" options={winnerOptions} />
-      <FastField id="time" name={`games[${game}].time`} as={TextInput} label="Time" />
+      <FastField id="time" name={`games[${game}].time`} as={TextInput} label="Time (minutes)" />
+      <FastField
+        id="gold1"
+        name={`games[${game}].teams[0].gold`}
+        as={TextInput}
+        label={`Gold (${values.games[game].teams[0].team.name})`}
+      />
+      <FastField
+        id="towers1"
+        name={`games[${game}].teams[0].towers`}
+        as={TextInput}
+        label={`Towers (${values.games[game].teams[0].team.name})`}
+      />
+      <FastField
+        id="gold2"
+        name={`games[${game}].teams[1].gold`}
+        as={TextInput}
+        label={`Gold (${values.games[game].teams[1].team.name})`}
+      />
+      <FastField
+        id="towers2"
+        name={`games[${game}].teams[1].towers`}
+        as={TextInput}
+        label={`Towers (${values.games[game].teams[1].team.name})`}
+      />
       <FastField id="screenshot" name={`games[${game}].screenshot`} as={TextInput} label="Screenshot" />
       <FastField id="matchHistory" name={`games[${game}].matchHistory`} as={TextInput} label="Match history" />
       <FastField id="breakdown" name={`games[${game}].breakdown`} as={TextInput} label="Breakdown" />
