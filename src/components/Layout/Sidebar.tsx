8c37b079fb7efa02d2e3e30d05babdedb768b5ca
amosbastian <amosbastian@gmail.com>
2020-04-12 15:19:30 +0200

    Add SidebarNavigation component with routes

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import Drawer, { DrawerProps } from "@material-ui/core/Drawer";
+import SidebarNavigation, { NavigationItem } from "./SidebarNavigation";
+import CreateIcon from "@material-ui/icons/Create";
+import EventIcon from "@material-ui/icons/Event";
+import GroupIcon from "@material-ui/icons/Group";
+import PersonIcon from "@material-ui/icons/Person";
+import EmojiEventsIcon from "@material-ui/icons/EmojiEvents";
 
 const StyledDrawer = styled(Drawer)`
   .MuiPaper-root {
@@ -12,10 +18,47 @@ const StyledDrawer = styled(Drawer)`
   }
 `;
 
+const DrawerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: ${({ theme }) => theme.spacing(2)}px;
+`;
+
+const navigationItems: NavigationItem[] = [
+  {
+    name: "PMTC",
+    to: "/",
+    icon: <CreateIcon />,
+  },
+  {
+    name: "Players",
+    to: "/players",
+    icon: <PersonIcon />,
+  },
+  {
+    name: "Teams",
+    to: "/teams",
+    icon: <GroupIcon />,
+  },
+  {
+    name: "Events",
+    to: "/events",
+    icon: <EmojiEventsIcon />,
+  },
+  {
+    name: "Schedule",
+    to: "/schedule",
+    icon: <EventIcon />,
+  },
+];
+
 const Sidebar: React.FC<DrawerProps> = ({ onClose, open, variant }) => {
   return (
     <StyledDrawer anchor="left" onClose={onClose} open={open} variant={variant}>
-      <div>Sidebar</div>
+      <DrawerContainer>
+        <SidebarNavigation navigationItems={navigationItems} />
+      </DrawerContainer>
     </StyledDrawer>
   );
 };
---
+++ src/components/Layout/SidebarNavigation.tsx
@@ -0,0 +1,75 @@
+import React, { forwardRef } from "react";
+import { NavLink as RouterLink } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+
+import { makeStyles } from "@material-ui/styles";
+import { colors } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: any) => ({
+  root: {},
+  item: {
+    display: "flex",
+    paddingTop: 0,
+    paddingBottom: 0,
+  },
+  button: {
+    color: colors.blueGrey[800],
+    padding: "0.625rem 1rem",
+    justifyContent: "flex-start",
+    textTransform: "none",
+    letterSpacing: 0,
+    width: "100%",
+    fontWeight: theme.typography.fontWeightMedium,
+  },
+  active: {
+    color: theme.palette.primary.main,
+    fontWeight: theme.typography.fontWeightMedium,
+    "& $icon": {
+      color: theme.palette.primary.main,
+    },
+  },
+}));
+
+const CustomRouterLink = forwardRef((props: any, ref: any) => (
+  <div ref={ref} style={{ flexGrow: 1 }}>
+    <RouterLink {...props} />
+  </div>
+));
+
+export type NavigationItem = {
+  icon: JSX.Element;
+  name: string;
+  to: string;
+};
+
+interface SidebarNavigationProps {
+  navigationItems: NavigationItem[];
+}
+
+const SidebarNavigation: React.FC<SidebarNavigationProps> = ({ navigationItems }) => {
+  const classes = useStyles();
+
+  return (
+    <List component="nav" aria-label="navigation">
+      {navigationItems.map(({ icon, name, to }) => (
+        <>
+          <ListItem className={classes.item} disableGutters>
+            <Button
+              activeClassName={classes.active}
+              className={classes.button}
+              component={CustomRouterLink}
+              to={to}
+              startIcon={icon}
+            >
+              {name}
+            </Button>
+          </ListItem>
+        </>
+      ))}
+    </List>
+  );
+};
+
+export default SidebarNavigation;
