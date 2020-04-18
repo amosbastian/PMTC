50d3ba78e779a44d764ac87d5b031f8f6c085081
amosbastian <amosbastian@gmail.com>
2020-04-19 01:54:53 +0200

    Add Formik to player form

--- src/components/Players/PlayerForm.tsx
+++ src/components/Players/PlayerForm.tsx
@@ -10,7 +10,8 @@ import { FetchPlayersQuery, FetchTeamsAndRolesQuery } from "../../generated/grap
 import { Button } from "@material-ui/core";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
-import { useForm, Controller } from "react-hook-form";
+import { Formik, Field, Form } from "formik";
+import FormikCheckbox from "../FormikCheckbox";
 
 export type Player = FetchPlayersQuery["players"][0];
 
@@ -51,62 +52,70 @@ interface PlayerFormProps {
 }
 
 const PlayerForm: React.FC<PlayerFormProps> = ({ handleClose, player }) => {
-  const methods = useForm();
-  const { handleSubmit, control, reset } = methods;
   const { data, loading, error } = useQuery<FetchTeamsAndRolesQuery>(FETCH_TEAMS_AND_ROLES);
   const onSubmit = (data: any) => {
     console.log(data);
   };
 
-  const teams = data?.teams.map((team) => ({ id: team.id, label: team.name }));
-  const roles = data?.roles.map((role) => ({ id: role.id, label: role.name }));
+  if (loading) return null;
 
-  const playerTeam = {
-    id: player?.team?.id ?? "",
-    label: player?.team?.name ?? "",
-  };
-
-  const playerRole = {
-    id: player?.role.id ?? "",
-    label: player?.role.name ?? "",
-  };
+  const teamIds = data!.teams.map((team) => team.id);
+  const roleIds = data!.roles.map((role) => role.id);
 
   return (
-    <Card component="form" onSubmit={handleSubmit(onSubmit)}>
-      <CardHeader title={player ? `Edit ${player.name}` : "Add player"} />
-      <StyledCardContent>
-        <Autocomplete
-          options={teams ?? []}
-          getOptionLabel={(option) => option?.label ?? ""}
-          filterSelectedOptions
-          renderInput={(params) => <TextField {...params} variant="outlined" label="Team" />}
-          disabled={!data}
-          defaultValue={player ? playerTeam : null}
-          size="small"
-        />
-        <Controller
-          as={<TextField fullWidth variant="outlined" label="Name" disabled={!data} size="small" />}
-          defaultValue={player?.name ?? ""}
-          name="name"
-          control={control}
-        />
-        <Autocomplete
-          options={roles ?? []}
-          getOptionLabel={(option) => option?.label ?? ""}
-          filterSelectedOptions
-          renderInput={(params) => <TextField {...params} variant="outlined" label="Role" />}
-          disabled={!data}
-          defaultValue={player ? playerRole : null}
-          size="small"
-        />
-      </StyledCardContent>
-      <StyledCardActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button type="submit" color="primary" variant="contained">
-          Save
-        </Button>
-      </StyledCardActions>
-    </Card>
+    <Formik
+      initialValues={{
+        name: "",
+        teamId: 0,
+        roleId: 0,
+        isStarter: false,
+      }}
+      onSubmit={(data) => console.log(data)}
+    >
+      {({ setFieldValue }) => (
+        <Card>
+          <CardHeader title={player ? `Edit ${player.name}` : "Add player"} />
+          <Form>
+            <StyledCardContent>
+              <Autocomplete
+                options={teamIds}
+                getOptionLabel={(option) => option.toString()}
+                filterSelectedOptions
+                renderInput={(params) => <TextField {...params} variant="outlined" label="Team" />}
+                disabled={!data}
+                size="small"
+                onChange={(event: unknown, value: any) => setFieldValue("teamId", value)}
+              />
+              <Field
+                name="name"
+                as={TextField}
+                fullWidth
+                variant="outlined"
+                label="Name"
+                disabled={!data}
+                size="small"
+              />
+              <Autocomplete
+                options={roleIds ?? []}
+                getOptionLabel={(option) => option.toString()}
+                filterSelectedOptions
+                renderInput={(params) => <TextField {...params} variant="outlined" label="Role" />}
+                disabled={!data}
+                size="small"
+                onChange={(event: unknown, value: any) => setFieldValue("roleId", value)}
+              />
+              <FormikCheckbox label="Starter" name="isStarter" />
+            </StyledCardContent>
+            <StyledCardActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button type="submit" color="primary" variant="contained">
+                Save
+              </Button>
+            </StyledCardActions>
+          </Form>
+        </Card>
+      )}
+    </Formik>
   );
 };
 
